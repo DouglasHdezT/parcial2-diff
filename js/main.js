85584493da9195eb8eb2678f34bad929a6698049
DouglasHdezT <00004516@uca.edu.sv>
2020-10-16 11:02:09 -0600

    Add: all little details

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const BASE_URL = "https://api.scryfall.com";
 
 const KEYS = {
-    SELECTED_KEY: "selected_key",
+    SELECTED_SET: "selected_set",
     DECK: "deck"
 }
 
@@ -12,33 +12,71 @@ const PATHS = {
     DECK: "/deck.html"
 }
 
+//Mains
+const indexMain = () => { 
+    fetchData(`${BASE_URL}/sets`, createAllSets);
+    document.querySelector(".deck-button>button").onclick = () => { 
+        window.location.href = "/deck.html"
+    }
+}
+
+const setMain = () => { 
+    const setInfo = JSON.parse(localStorage.getItem(KEYS.SELECTED_SET));
+
+    if (!setInfo) { 
+        document.querySelector("title").innerText = "No selected set";
+        document.querySelector("#title").innerText = "You don't select any set of cards";
+        return;
+    }
+
+    document.querySelector("title").innerText = `Set: ${setInfo.name}`;
+    document.querySelector("#title").innerText = setInfo.name;
+
+    const { cardsUri } = setInfo;
+
+    fetchData(cardsUri, createAllCards);
+}
+
+const deckMain = () => { 
+    const deck = JSON.parse(localStorage.getItem(KEYS.DECK));
+    createAllDeckCards(deck);
+}
+
 //On load
 window.onload = () => { 
     switch (window.location.pathname) {
         case PATHS.INDEX:
         case PATHS.ROOT:
-            fetchData(`${BASE_URL}/sets`, createAllSets);
+            indexMain();
             break;
         case PATHS.SET:
-            
+            setMain();
             break;
         case PATHS.DECK:
+            deckMain();
             break
         default:
             break;
     }
+
+    document.addEventListener("keyup", (event) => {
+        if (event.code === "Enter") { 
+            const item = document.querySelector(":focus");
+            if (item) item.click();
+        }
+    });
     
 }
 
 //Tratamiento para sets
 
 const createSetCard = (setInfo) => { 
-    const { name, releaseDate, cards, code, cardsUri, img } = setInfo;
+    const { name, releaseDate, cards, code, img } = setInfo;
     
     const container = document.createElement("article");
     container.classList.add("set-card");
-    container.onclick = () => { 
-        localStorage.setItem(KEYS.SELECTED_KEY, JSON.stringify(setInfo));
+    container.onclick = () => {
+        localStorage.setItem(KEYS.SELECTED_SET, JSON.stringify(setInfo));
         window.location.href = "/set.html"
     }
 
@@ -80,13 +118,168 @@ const parseSetInfo = (rawSet) => {
 }
 
 const createAllSets = async ({ data:rawSets }) => { 
-    const container = document.querySelector("#cards-container>#items");
+    const container = document.querySelector(".cards-container>.items");
     container.innerHTML = "";
 
     rawSets.forEach(rawSet => { 
         const card = createSetCard(parseSetInfo(rawSet));
         container.appendChild(card);
     });
+}
+
+//Tratamiento para cards
+const createCardCard = (cardInfo) => { 
+    const { img, name, id } = cardInfo;
+
+    const container = document.createElement("article");
+    container.classList.add("card-card");
+    container.dataset.id = id
+
+    const { code } = JSON.parse(localStorage.getItem(KEYS.SELECTED_SET)) || undefined;
+
+    if (isCardInDeck(id, code)) { 
+        container.classList.add("decked")
+    }
+
+    container.onclick = () => { 
+        toggleCardInDeck(cardInfo, code);
+    }
+
+    const cardHTML = `
+        <img src="${img}" alt="${name}">
+        <img class="check" src="./img/check.png" alt="Cheque"/>
+        <div class="add"> <p>Add</p> </div>
+        <div class="remove"> <p>Remove</p> </div>
+
+    `;
+
+    container.innerHTML = cardHTML;
+    return container;
+}
+
+const parseCardInfo = (rawCard) => { 
+    const { id, name, image_uris } = rawCard;
+    return {
+        id,
+        name,
+        img: image_uris.border_crop
+    }
+}
+
+const createAllCards = async ({ data: rawCards}) => { 
+    const container = document.querySelector(".cards-container>.items");
+    container.innerHTML = "";
+
+    rawCards.forEach(rawCard => {
+        const card = createCardCard(parseCardInfo(rawCard));
+        container.appendChild(card);
+    });
+}
+
+const isSetInDeck = (code) => { 
+    const deck = JSON.parse(localStorage.getItem(KEYS.DECK));
+    if (!deck) return false;
+
+    const set = deck[code];
+    return set;
+}
+
+const isCardInDeck = (id, code) => { 
+    if (!code) return false;
+
+    const set = isSetInDeck(code);
+    if (!set) return false
+    
+    const card = set[id];
+    return card;
+}
+
+const toggleCardInDeck = (card, code) => { 
+    const deck = JSON.parse(localStorage.getItem(KEYS.DECK)) || {};
+
+    if (!isSetInDeck(code)) {
+        deck[code] = {};
+    }
+
+    const cardCard = document.querySelector(`[data-id = "${card.id}"]`);
+
+    if (!isCardInDeck(card.id, code)) {
+        addCardToDeck(card, code, deck);
+        cardCard.classList.add("decked");
+    } else { 
+        removeCardInDeck(card, code, deck);
+        cardCard.classList.remove("decked");
+    }    
+}
+
+const addCardToDeck = (card, setCode,  deck) => { 
+    deck[setCode][card.id] = card;
+    localStorage.setItem(KEYS.DECK, JSON.stringify(deck));
+}
+
+const removeCardInDeck = (card, setCode, deck) => { 
+    deck[setCode][card.id] = undefined;
+    localStorage.setItem(KEYS.DECK, JSON.stringify(deck));
+}
+
+//Deck management
+
+const createCardOnDeck = (cardInfo, setCode) => { 
+    const { img, name, id } = cardInfo;
+
+    const container = document.createElement("article");
+    container.classList.add("card-card");
+    container.dataset.id = id
+    container.tabIndex = 0;
+    container.setAttribute("role", "link")
+
+    if (isCardInDeck(id, setCode)) { 
+        container.classList.add("decked")
+    }
+
+    container.onclick = () => { 
+        removeCardInDeck(cardInfo, setCode, JSON.parse(localStorage.getItem(KEYS.DECK)));
+        container.parentElement.removeChild(container);
+    }
+
+    const cardHTML = `
+        <img src="${img}" alt="${name}">
+        <div class="add"> <p>Add</p> </div>
+        <div class="remove"> <p>Remove</p> </div>
+    `;
+
+    container.innerHTML = cardHTML;
+    return container;
+}
+
+const createAllDeckCards = (deck) => { 
+    const container = document.querySelector(".sets-in-deck");
+    container.innerHTML = "";
+
+    Object.keys(deck).forEach(set => { 
+        if (Object.keys(deck[set]).length === 0) return;
+
+        const section = document.createElement("section");
+        section.innerHTML = `
+        <section class="cards-container">
+            <h2 style = "text-transform: uppercase;"> ${set} </h2>
+            
+            <div class="items">
+            </div>
+        </section>
+        `;
+
+        section.dataset.set = set;
+
+        const items = section.querySelector(".items");
+
+        Object.keys(deck[set]).forEach(cardId => { 
+            const card = createCardOnDeck(deck[set][cardId], set);
+            items.appendChild(card);
+        });
+
+        container.appendChild(section);
+    })
 }
 
 const fetchData = async (uri, action) => { 
